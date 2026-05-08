156ad35dd35f8e320a05ab688ebe9d11b2a52125
Cao Vo Quang Minh <caovoquangminh@gmail.com>
2026-05-08 15:08:16 +0700

    feat: init project

---
+++ src/main/java/netcoffee/NetcoffeeApplication.java
@@ -0,0 +1,13 @@
+package netcoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetcoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetcoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/netcoffee/NetcoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package netcoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetcoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
